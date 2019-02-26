ce93abd044666e5e317e7da2b42d4526c0078c03
csy512889371 <32060663+csy512889371@users.noreply.github.com>
2019-02-26 15:56:23 +0800

    baseresponse
    baseresponse

--- rjsoft-jycj-admin-web/src/main/java/com/rjsoft/jycj/admin/web/controller/DemoController.java
+++ rjsoft-jycj-admin-web/src/main/java/com/rjsoft/jycj/admin/web/controller/DemoController.java
@@ -1,7 +1,10 @@
 package com.rjsoft.jycj.admin.web.controller;
 
+import com.rjsoft.magina.spi.common.dto.BaseResponse;
 import com.rjsoft.uums.service.provider.dto.BlogTopicDto;
 import com.rjsoft.uums.service.provider.spi.BlogTopicSpi;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +14,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/demo")
+@Api(value = "demo", description = "例子-demo")
 public class DemoController {
 
     @Autowired
     private BlogTopicSpi blogTopicSpi;
 
     @GetMapping(value = "/echoSuccess")
-    public List<BlogTopicDto> echoSuccess() {
-        List<BlogTopicDto> blogTopicList = blogTopicSpi.getBlogTopicList();
-        return blogTopicList;
+    @ApiOperation("test echoSuccess")
+    public BaseResponse<List<BlogTopicDto>> echoSuccess() {
+        BaseResponse<List<BlogTopicDto>> blogTopicListResp = blogTopicSpi.getBlogTopicList();
+        return blogTopicListResp;
     }
 }
