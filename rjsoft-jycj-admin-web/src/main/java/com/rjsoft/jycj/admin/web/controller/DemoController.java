52430ecb315f7e27de306aa1a7c73f8f9ab00362
csy512889371 <32060663+csy512889371@users.noreply.github.com>
2019-02-26 14:58:24 +0800

    feign demo
    feign demo

--- rjsoft-jycj-admin-web/src/main/java/com/rjsoft/jycj/admin/web/controller/DemoController.java
+++ rjsoft-jycj-admin-web/src/main/java/com/rjsoft/jycj/admin/web/controller/DemoController.java
@@ -1,15 +1,24 @@
 package com.rjsoft.jycj.admin.web.controller;
 
+import com.rjsoft.uums.service.provider.dto.BlogTopicDto;
+import com.rjsoft.uums.service.provider.spi.BlogTopicSpi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/demo")
 public class DemoController {
 
+    @Autowired
+    private BlogTopicSpi blogTopicSpi;
+
     @GetMapping(value = "/echoSuccess")
-    public String echoSuccess() {
-        return "成功";
+    public List<BlogTopicDto> echoSuccess() {
+        List<BlogTopicDto> blogTopicList = blogTopicSpi.getBlogTopicList();
+        return blogTopicList;
     }
 }
